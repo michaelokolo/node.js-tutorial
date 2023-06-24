3d38302cb18539d9ecb48e4cab0ea3f894847709
Michael Okolo <michaelokolo62@yahoo.com>
2023-06-24 16:31:20 -0400

    using require and export in modules

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// GLOBALS - NO WINDOW !!!!
+// Modules
 
-// __dirname - path to current directory
-// __filename - file name
-// require    - function to use modules (CommonJS)
-// module     - info about current module (file)
-// process    - info about env where the program is being executed
+const names = require('./4-names')
+const sayHi = require('./5-utils')
 
 
-setInterval(()=>{console.log('Michael')},1000)
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
