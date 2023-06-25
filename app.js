4cbafe974feefa746a776bdb3098ec1664459275
Michael Okolo <michaelokolo62@yahoo.com>
2023-06-25 19:50:50 -0400

    understanding jason package better

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const {readFile, writeFile } = require('fs')
+// npm - global command, comes with node
+// npm --version
 
-readFile('./content/first.txt', 'utf8', (err, result)=>{
-    if (err) {
-        console.log(err)
-        return
-    }
-    const first = result;
-    readFile('./content/second.txt', 'utf8', (err,result)=>{
-        if (err){
-            console.log(err)
-            return
-        }
-        const second = result;
-    })
-})
+// local dependency - use it only in this particular project
+// npm i <packageName>
+
+// global dependency -- use it in any project
+// npm install -g <packageName>
+// sudo install -g <packageName> (mac)
+
+// package.json - manifest file (stores important info about project/package)'
+// manual approach (create package.json in the root, create properties etc)
+// npm init (step by step, press enter to skip)
+// npm init -y (everything default)
+
+
+const _ = require('lodash')
+
+const items = [1, [2, [3, [4]]]]
+const newItems = _.flattenDeep(items)
+console.log(newItems)
