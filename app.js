f7aa0a34bd2412870004933bc436657d12b05328
Michael Okolo <michaelokolo62@yahoo.com>
2023-06-28 05:33:32 -0400

    async and promises

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-const {readFile} = require('fs')
+const {readFile, writeFile} = require('fs').promises
+// const util = require('util')
+// const readFilePromise = util.promisify(readFile)
+// const writeFilePromise = util.promisify(writeFile)
 
-const getText = (path) =>{
-    return new Promise((resolve,reject)=>{
-        readFile(path,'utf8', (err,data)=>{
-            if(err){
-                reject(err)
-            }else{
-                resolve(data)
-            }
-        })
-    })
+
+const start = async() =>{
+    try {
+        const first = await readFile('./content/first.txt','utf8');
+        const second = await readFile('./content/second.txt','utf8');
+        await writeFile('./content/result-mind-grenade.txt', 
+        `THIS IS AWESOME: ${first} ${second}`,{flag:'a'})
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+    }   
 }
 
-getText('./content/second.txt')
-.then((result) => console.log(result))
-.catch((err)=> console.log(err))
+start()
+
+// const getText = (path) =>{
+//     return new Promise((resolve,reject)=>{
+//         readFile(path,'utf8', (err,data)=>{
+//             if(err){
+//                 reject(err)
+//             }else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/second.txt')
+// .then((result) => console.log(result))
+// .catch((err)=> console.log(err))
